c03178274239846681fef460c3de4535c6640451
Aitorbp1 <101257184+Aitorbp1@users.noreply.github.com>
2022-04-02 23:46:13 +0200

    Falta Ultima escena

---
+++ Escenes/Cossos/PesosnatgeCos.gd
@@ -0,0 +1,63 @@
+extends KinematicBody2D
+
+var velocitat_base = 200 #la velocitat inicial
+var velocitat = Vector2.ZERO
+var direccio = Vector2.DOWN
+var gravetat = Vector2.DOWN * 980
+var velocitat_salt = -340
+
+func _physics_process(delta):
+	velocitat.x = 0
+	velocitat += gravetat * delta
+	if Input.is_action_pressed("mou dreta"):
+		velocitat += Vector2.RIGHT * velocitat_base
+	if Input.is_action_pressed("mou esquerra"):
+		velocitat += Vector2.LEFT * velocitat_base
+	if Input.is_action_just_pressed("mou amunt") and is_on_floor():
+		velocitat.y = velocitat_salt
+		
+	
+	
+	velocitat = move_and_slide(velocitat, Vector2.UP)
+	animation(velocitat)
+func animation(velocitat):
+	if velocitat.x > 0 and velocitat.y > -2:
+		$Alieen.play("Camina")
+		$Alieen.flip_h = false
+	elif velocitat.x < 0 and velocitat.y > -2:
+		$Alieen.play("Camina")
+		$Alieen.flip_h = true
+		
+	if abs(velocitat.x) < 0.1 and velocitat.y > -1:
+		$Alieen.play("Quiet")
+		
+	if velocitat.y < -2:
+		$Alieen.play("Salta")
+
+ 
+
+
+func _on_Respawn_body_entered(body):
+	position = Vector2(-133, 149)
+
+	
+
+
+
+
+
+
+
+func _on_Area2D_body_entered(body):
+	get_tree().change_scene("res://Escenes/ESCENA2.tscn")
+
+
+func _on_Respawn_1_body_entered(body):
+	position = Vector2(-123, -31)
+
+
+	
+
+
+func _on_Area2D4_body_entered(body):
+	get_tree().change_scene("res://Escenes/ESCENA3.tscn")
---
+++ Escenes/Pinxo.gd
@@ -0,0 +1,20 @@
+extends Sprite
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+#export var velocitat_mov = 5
+#export var velocitat_max = 25
+#var gravetat = 250
+#var velocitat = Vector2()
+
+
+#func _ready():
+	#pass 
+
+#func _physics_process(delta):
+	locitat.y += gravetat * delta
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
